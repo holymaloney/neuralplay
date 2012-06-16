a2bb7cf5fc4324008aded222bf1da41ac784b969
Hans Petter Simonsen <hans.petter.simonsen@gmail.com>
2012-06-16 23:24:56 +0200

    Initial commit
    Playing with neural networks

---
+++ src/main/java/org/hps/neuralplay/App.java
@@ -0,0 +1,13 @@
+package org.hps.neuralplay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
